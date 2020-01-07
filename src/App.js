38fa55d0a1927c7652c365b4641a5992ae5ce24b
maher127 <maher_hedhili@hotmai.fr>
2020-01-07 15:21:11 +0100

    add header and link to home,shop page and contact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
+import Shop from "./pages/shop/Shop";
+import "./App.css";
+import Header from "./Components/header/Header";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={Shop} />
+      </Switch>
     </div>
   );
 }
---
+++ src/Components/preview-collection/PreviewCollection.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./preview-collection.scss";
+import CollectionItem from "../collection-item/CollectionItem";
+
+export default function PreviewCollection({ items, title }) {
+  return (
+    <div className="collection-preview">
+      <h1 className="title ">{title.toUpperCase()}</h1>
+      <div className="preview ">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
+          ))}
+      </div>
+    </div>
+  );
+}
